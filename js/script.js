090f942c40a10ed969038eef3ff727c5378086f5
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-04-09 22:41:15 +0300

    верстка сайта закончена

--- js/script.js
+++ js/script.js
@@ -20,3 +20,15 @@ function open(e) {
 	});
 	document.querySelector(`#${activeTab}`).classList.add("choose__item--active");
 }
+ 
+const menuBtn = document.querySelector('.menu__btn')
+const menuList = document.querySelector('.menu__list')
+// const body= document.querySelector('body')
+menuBtn.addEventListener('click',(e)=>{
+	
+	menuBtn.classList.toggle('menu__btn--active')
+	menuList.classList.toggle('menu__list--active')
+	document.body.classList.toggle('ovh')
+console.log(document.body);
+	}
+)
